542494a3643aa44abca765566dade07128726bf9
Dilshod Temirkhodjaev <tdilshod@gmail.com>
2010-10-11 13:43:27 +0400

    better date/time formatting, columns delimiter option, and multisheet support

--- xlsx2csv.py
+++ xlsx2csv.py
@@ -5,32 +5,149 @@ __author__ = "Dilshod Temirkhodjaev <tdilshod@gmail.com>"
 import csv, datetime, zipfile, sys
 import xml.parsers.expat
 from xml.dom import minidom
+from optparse import OptionParser
+
+# see also ruby-roo lib at: http://github.com/hmcgowan/roo
+FORMATS = {
+  'general' : 'float',
+  '0' : 'float',
+  '0.00' : 'float',
+  '#,##0' : 'float',
+  '#,##0.00' : 'float',
+  '0%' : 'percentage',
+  '0.00%' : 'percentage',
+  '0.00e+00' : 'float',
+  'mm-dd-yy' : 'date',
+  'd-mmm-yy' : 'date',
+  'd-mmm' : 'date',
+  'mmm-yy' : 'date',
+  'h:mm am/pm' : 'date',
+  'h:mm:ss am/pm' : 'date',
+  'h:mm' : 'time',
+  'h:mm:ss' : 'time',
+  'm/d/yy h:mm' : 'date',
+  '#,##0 ;(#,##0)' : 'float',
+  '#,##0 ;[red](#,##0)' : 'float',
+  '#,##0.00;(#,##0.00)' : 'float',
+  '#,##0.00;[red](#,##0.00)' : 'float',
+  'mm:ss' : 'time',
+  '[h]:mm:ss' : 'time',
+  'mmss.0' : 'time',
+  '##0.0e+0' : 'float',
+  '@' : 'float',
+  'yyyy\\-mm\\-dd' : 'date',
+  'dd/mm/yy' : 'date',
+  'hh:mm:ss' : 'time',
+  "dd/mm/yy\\ hh:mm" : 'date',
+  'yy-mm-dd' : 'date',
+  'd-mmm-yyyy' : 'date',
+  'mm/dd/yy hh:mm am/pm' : 'date',
+  'mm/dd/yyyy hh:mm:ss' : 'date',
+}
+STANDARD_FORMATS = { 
+  0 : 'general',
+  1 : '0',
+  2 : '0.00',
+  3 : '#,##0',
+  4 : '#,##0.00',
+  9 : '0%',
+  10 : '0.00%',
+  11 : '0.00e+00',
+  12 : '# ?/?',
+  13 : '# ??/??',
+  14 : 'mm-dd-yy',
+  15 : 'd-mmm-yy',
+  16 : 'd-mmm',
+  17 : 'mmm-yy',
+  18 : 'h:mm am/pm',
+  19 : 'h:mm:ss am/pm',
+  20 : 'h:mm',
+  21 : 'h:mm:ss',
+  22 : 'm/d/yy h:mm',
+  37 : '#,##0 ;(#,##0)',
+  38 : '#,##0 ;[red](#,##0)',
+  39 : '#,##0.00;(#,##0.00)',
+  40 : '#,##0.00;[red](#,##0.00)',
+  45 : 'mm:ss',
+  46 : '[h]:mm:ss',
+  47 : 'mmss.0',
+  48 : '##0.0e+0',
+  49 : '@',
+}
 
 #
-# example: xlsx2csv("test.xslx", open("test.csv", "w+"))
+# usage: xlsx2csv("test.xslx", open("test.csv", "w+"))
+# parameters:
+#   sheetid - sheet no to convert (0 for all sheets)
+#   dateformat - override date/time format
+#   delimiter - csv columns delimiter symbol
+#   sheet_delimiter - sheets delimiter used when processing all sheets
 #
-def xlsx2csv(infilepath, outfile):
-    writer = csv.writer(outfile, quoting=csv.QUOTE_MINIMAL)
+def xlsx2csv(infilepath, outfile, sheetid=1, dateformat=None, delimiter=",", sheetdelimiter="--------"):
+    writer = csv.writer(outfile, quoting=csv.QUOTE_MINIMAL, delimiter=delimiter)
     ziphandle = zipfile.ZipFile(infilepath)
-    shared_strings = SharedStrings(ziphandle.read("xl/sharedStrings.xml"))
+    try:
+        shared_strings = parse(ziphandle, SharedStrings, "xl/sharedStrings.xml")
+        styles = parse(ziphandle, Styles, "xl/styles.xml")
+        workbook = parse(ziphandle, Workbook, "xl/workbook.xml")
 
-    # multisheet:
-    #self.workbook = Workbook(ziphandle.read("xl/workbook.xml"))
-    #for i in self.workbook.sheets:
-    #    SharedStrings(ziphandle.read("xl/worksheets/sheet%s.xml" %(i['id'])))
+        if sheetid > 0:
+            sheet = None
+            for s in workbook.sheets:
+                if s['id'] == sheetid:
+                    sheet = Sheet(shared_strings, styles, ziphandle.read("xl/worksheets/sheet%i.xml" %s['id']))
+                    break
+            if not sheet:
+                raise Exception("Sheet %i Not Found" %sheetid)
+            sheet.set_dateformat(dateformat)
+            sheet.to_csv(writer)
+        else:
+            for s in workbook.sheets:
+                outfile.write(sheetdelimiter + " " + str(s['id']) + " - " + s['name'] + "\r\n")
+                sheet = Sheet(shared_strings, styles, ziphandle.read("xl/worksheets/sheet%i.xml" %s['id']))
+                sheet.set_dateformat(dateformat)
+                sheet.to_csv(writer)
+    finally:
+        ziphandle.close()
 
-    Sheet(shared_strings, ziphandle.read("xl/worksheets/sheet1.xml"), writer)
-    ziphandle.close()
+def parse(ziphandle, klass, filename):
+    instance = klass()
+    if filename in ziphandle.namelist():
+        instance.parse(ziphandle.read(filename))
+    return instance
 
-class Workbook(object):
+class Workbook:
     sheets = []
-    def __init__(self, data):
+
+    def parse(self, data):
         workbookDoc = minidom.parseString(data)
         sheets = workbookDoc.firstChild.getElementsByTagName("sheets")[0]
         for sheetNode in sheets.childNodes:
-            name = sheetNode._attrs["name"].value
-            id = int(sheetNode._attrs["r:id"].value[3:])
+            attrs = sheetNode._attrs
+            name = attrs["name"].value
+            if attrs.has_key('sheetId'):
+                id = int(attrs['sheetId'].value)
+            else:
+                id = int(attrs["r:id"].value[3:])
             self.sheets.append({'name': name, 'id': id})
+
+class Styles:
+    numFmts = {}
+    cellXfs = []
+
+    def parse(self, data):
+        styles = minidom.parseString(data).firstChild
+        # numFmts
+        numFmtsElement = styles.getElementsByTagName("numFmts")[0]
+        for numFmt in numFmtsElement.childNodes:
+            numFmtId = int(numFmt._attrs['numFmtId'].value)
+            formatCode = numFmt._attrs['formatCode'].value.lower().replace('\\', '')
+            self.numFmts[numFmtId] = formatCode
+        # cellXfs
+        cellXfsElement = styles.getElementsByTagName("cellXfs")[0]
+        for cellXfs in cellXfsElement.childNodes:
+            numFmtId = int(cellXfs._attrs['numFmtId'].value)
+            self.cellXfs.append(numFmtId)
 
 class SharedStrings:
     parser = None
@@ -39,7 +156,7 @@ class SharedStrings:
     t = False
     value = ""
 
-    def __init__(self, data):
+    def parse(self, data):
         self.parser = xml.parsers.expat.ParserCreate()
         self.parser.CharacterDataHandler = self.handleCharData
         self.parser.StartElementHandler = self.handleStartElement
@@ -47,7 +164,8 @@ class SharedStrings:
         self.parser.Parse(data)
 
     def handleCharData(self, data):
-        if self.t: self.value+= data
+        if self.t:
+            self.value+= data
 
     def handleStartElement(self, name, attrs):
         if name == 'si':
@@ -67,6 +185,7 @@ class Sheet:
     parser = None
     writer = None
     sharedString = None
+    styles = None
 
     in_sheet = False
     in_row = False
@@ -80,44 +199,63 @@ class Sheet:
     s_attr = None
     data = None
 
-    def __init__(self, sharedString, data, writer):
-        self.writer = writer
+    dateformat = None
+
+    def __init__(self, sharedString, styles, data):
+        self.data = data
         self.sharedStrings = sharedString.strings
+        self.styles = styles
+
+    def set_dateformat(self, dateformat):
+        self.dateformat = dateformat
+
+    def to_csv(self, writer):
+        self.writer = writer
         self.parser = xml.parsers.expat.ParserCreate()
         self.parser.CharacterDataHandler = self.handleCharData
         self.parser.StartElementHandler = self.handleStartElement
         self.parser.EndElementHandler = self.handleEndElement
-        self.parser.Parse(data)
+        self.parser.Parse(self.data)
 
     def handleCharData(self, data):
         if self.in_cell_value:
-            if self.colType == "s":
-                # shared
+            self.data = data # default value
+            if self.colType == "s": # shared string
                 self.data = self.sharedStrings[int(data)]
-            #elif self.colType == "b": # boolean
+            elif self.colType == "b": # boolean
+                self.data = (int(data) == 1 and "TRUE") or (int(data) == 0 and "FALSE") or data
             elif self.s_attr:
-                if self.s_attr == '2':
-                    # date
-                    try:
-                        self.data = (datetime.date(1899, 12, 30) + datetime.timedelta(float(data))).strftime("%m/%d/%y")
-                    except (ValueError, OverflowError):
-                        # invalid date format
-                        self.data = data
-                elif self.s_attr == '3':
-                    # time
-                    self.data = str(float(data) * 24*60*60)
-                    # datetime
-                elif self.s_attr == '1':
-                    try:
-                        self.data = (datetime.datetime(1899, 12, 30) + datetime.timedelta(float(data))).strftime("%m/%d/%y %H:%M")
-                    except (ValueError, OverflowError):
-                        # invalid date format
-                        self.data = data
-                else:
-                    self.data = data
-            else:
-                self.data = data
-        # does not support it yet
+                s = int(self.s_attr)
+
+                # get cell format
+                format = None
+                xfs_numfmt = self.styles.cellXfs[s]
+                if self.styles.numFmts.has_key(xfs_numfmt):
+                    format = self.styles.numFmts[xfs_numfmt]
+                elif STANDARD_FORMATS.has_key(xfs_numfmt):
+                    format = STANDARD_FORMATS[xfs_numfmt]
+                # get format type
+                if format and FORMATS.has_key(format):
+                    format_type = FORMATS[format]
+
+                    if format_type == 'date': # date/time
+                        try:
+                            date = datetime.date(1899, 12, 30) + datetime.timedelta(float(data))
+                            if self.dateformat:
+                                self.data = date.strftime(self.dateformat)
+                            else:
+                                dateformat = format.replace("yyyy", "%Y").replace("yy", "%y"). \
+                                  replace("hh:mm", "%h:%M").replace("hh", "%h").replace("ss", "%S"). \
+                                  replace("d", "%e").replace("%e%e", "%d"). \
+                                  replace("mmmm", "%B").replace("mmm", "%b").replace("mm", "%m"). \
+                                  replace("am/pm", "%p")
+                                self.data = date.strftime(dateformat).strip()
+                        except (ValueError, OverflowError):
+                            # invalid date format
+                            self.data = data
+                    elif format_type == 'time': # time
+                        self.data = str(float(data) * 24*60*60)
+        # does not support it
         #elif self.in_cell_formula:
         #    self.formula = data
 
@@ -152,20 +290,51 @@ class Sheet:
             self.columns[t] = self.data
             self.in_cell = False
         if self.in_row and name == 'row':
-            d = [""] * (max(self.columns.keys()) + 1)
-            for k in self.columns.keys():
-                d[k] = self.columns[k].encode("utf-8")
-            self.writer.writerow(d)
+            if len(self.columns.keys()) > 0:
+                d = [""] * (max(self.columns.keys()) + 1)
+                for k in self.columns.keys():
+                    d[k] = self.columns[k].encode("utf-8")
+                self.writer.writerow(d)
             self.in_row = False
         elif self.in_sheet and name == 'sheetData':
             self.in_sheet = False
 
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        xlsx2csv(sys.argv[1], sys.stdout)
-    elif len(sys.argv) == 3:
-        f = open(sys.argv[2], "w+")
-        xlsx2csv(sys.argv[1], f)
+    parser = OptionParser(usage = "%prog [options] infile [outfile]", version="0.1")
+    parser.add_option("-s", "--sheet", dest="sheetid", default=1, type="int",
+      help="sheet no to convert (0 for all sheets)")
+    parser.add_option("-d", "--delimiter", dest="delimiter", default=",",
+      help="delimiter - csv columns delimiter, 'tab' or 'x09' for tab (comma is default)")
+    parser.add_option("-p", "--sheetdelimiter", dest="sheetdelimiter", default="--------",
+      help="sheets delimiter used when processing all sheets (default '--------')")
+    parser.add_option("-f", "--dateformat", dest="dateformat",
+      help="override date/time format (ex. %Y/%m/%d)")
+
+    (options, args) = parser.parse_args()
+
+    if len(options.delimiter) == 1:
+        delimiter = options.delimiter
+    elif options.delimiter == 'tab':
+        delimiter = '\t'
+    elif options.delimiter == 'comma':
+        delimiter = ','
+    elif options.delimiter[0] == 'x':
+        delimiter = chr(int(options.delimiter[1:]))
+    else:
+        raise Exception("Invalid delimiter")
+
+    kwargs = {
+      'sheetid' : options.sheetid,
+      'delimiter' : delimiter,
+      'sheetdelimiter' : options.sheetdelimiter,
+      'dateformat' : options.dateformat
+    }
+
+    if len(args) == 1:
+        xlsx2csv(args[0], sys.stdout, **kwargs)
+    elif len(args) == 2:
+        f = open(args[1], "w+")
+        xlsx2csv(args[0], f, **kwargs)
         f.close()
     else:
-        print "Usage: xlsx2csv <infile> [<outfile>]"
+        parser.print_usage()
