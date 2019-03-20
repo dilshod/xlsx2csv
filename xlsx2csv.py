28c13b41930a6100573ee1a6188033cbea15e3a5
Dilshod <tdilshod@gmail.com>
2019-03-20 21:20:25 +0500

    use in instead of has_key

--- xlsx2csv.py
+++ xlsx2csv.py
@@ -207,7 +207,7 @@ class Xlsx2csv:
     def getSheetIdByName(self, name):
         for s in self.workbook.sheets:
             if s['name'] == name:
-                return s.has_key('index') and s['index'] or s['id']
+                return 'index' in s and s['index'] or s['id']
         return None
 
     def convert(self, outfile, sheetid=1):
@@ -222,7 +222,7 @@ class Xlsx2csv:
                     raise OutFileAlreadyExistsException("File " + str(outfile) + " already exists!")
             for s in self.workbook.sheets:
                 sheetname = s['name']
-                sheetindex = s.has_key('index') and s['index'] or s['id']
+                sheetindex = 'index' in s and s['index'] or s['id']
 
                 # filter sheets by include pattern
                 include_sheet_pattern = self.options['include_sheet_pattern']
@@ -274,7 +274,7 @@ class Xlsx2csv:
             writer = csv.writer(outfile, quoting=self.options['quoting'], delimiter=self.options['delimiter'],
                                 lineterminator=self.options['lineterminator'])
 
-            sheets_filtered = list(filter(lambda s: (s.has_key('index') and s['index'] or s['id']) == sheet_index, self.workbook.sheets))
+            sheets_filtered = list(filter(lambda s: ('index' in s and s['index'] or s['id']) == sheet_index, self.workbook.sheets))
             if len(sheets_filtered) == 0:
                 eprint("Sheet with index %i not found or can't be handled" % sheet_index)
                 return 1
