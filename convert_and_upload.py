406b20e62c9a231368fbc23a9dea4125b8777b2a
MalachiteMiller <malachitehmiller@gmail.com>
2023-05-18 20:10:28 -0700

    Add ability to download source files from cloud storage

--- convert_and_upload.py
+++ convert_and_upload.py
@@ -9,22 +9,29 @@ import pandas as pd
 from xlsx2csv import convert_recursive
 
 
-def convert_and_upload(source, bq_location):
+def convert_and_upload(bq_dest, local_source, c_storage_source):
     bq_load_options = "--autodetect --allow_quoted_newlines=true --schema_update_option=ALLOW_FIELD_ADDITION " \
                       "--schema_update_option=ALLOW_FIELD_RELAXATION --source_format=CSV"
     pattern = r"(\d{4})(\d{2})(\d{2})"
 
-    for i in range(1, 50):
-        if os.path.exists(f'temp_{i}'):
-            continue
-        else:
-            os.mkdir(f'temp_{i}')
-            output_dir = f'temp_{i}'
-            break
-    else:
-        raise OSError('All temp directories are in use')
+    output_dir = find_temp_dir()
 
-    convert_recursive(source, 1, output_dir, {"escape_strings": True})
+    if c_storage_source:
+        input_source = find_temp_dir()
+        if c_storage_source.split('.')[-1] != 'xlsx':
+            ending = '*.xlsx'
+            if c_storage_source[-1] != '/':
+                ending = '/*.xlsx'
+            c_storage_source = c_storage_source + ending
+        os.system(f"gcloud storage cp {c_storage_source} {input_source}")
+        if len(os.listdir(input_source)) == 0:
+            raise OSError(f"No xlsx files found at {c_storage_source}")
+    elif local_source:
+        input_source = local_source
+    else:
+        raise RuntimeError("No source specified")
+
+    convert_recursive(input_source, 1, output_dir, {"escape_strings": True})
 
     with os.scandir(output_dir) as it:
         for file in it:
@@ -45,6 +52,8 @@ def convert_and_upload(source, bq_location):
     os.system(f"gcloud storage cp {output_dir}/*.csv gs://skynamo_history/temp")
 
     shutil.rmtree(output_dir)
+    if c_storage_source:
+        shutil.rmtree(input_source)
 
     cmd = ['gcloud', 'storage', 'ls', 'gs://skynamo_history/temp/']
     result = subprocess.run(cmd, capture_output=True, text=True)
@@ -54,16 +63,31 @@ def convert_and_upload(source, bq_location):
         files = files.splitlines()
         for i, file in enumerate(files):
             print(f"{i + 1}/{len(files)}")
-            os.system(f"bq load {bq_load_options} {bq_location} {file}")
+            os.system(f"bq load {bq_load_options} {bq_dest} {file}")
     else:
         print(result.stderr)
 
     os.system("gcloud storage rm -r gs://skynamo_history/temp/")
 
 
+def find_temp_dir():
+    for i in range(1, 50):
+        if os.path.exists(f'temp_{i}'):
+            continue
+        else:
+            os.mkdir(f'temp_{i}')
+            output_dir = f'temp_{i}'
+            return output_dir
+    else:
+        raise OSError('All temp directories are in use')
+
+
 if __name__ == '__main__':
     parser = ArgumentParser(description='Upload a directory of xlsx files to a BigQuery table')
-    parser.add_argument('-d', '--directory', help='The directory')
     parser.add_argument('-t', '--table', help='The output dataset and table name: "dataset.table"')
+    parser.add_argument('-l', '--local-source', dest='local_source', default=None,
+                        help='The local directory source: /path/to/directory')
+    parser.add_argument('-c', '--cloud-source', dest='cloud_source', default=None,
+                        help='The cloud storage source: gs://bucket/path')
     args = parser.parse_args()
-    convert_and_upload(args.directory, args.table)
+    convert_and_upload(args.table, args.local_source, args.cloud_source)
