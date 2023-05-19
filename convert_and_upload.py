daeeb2562eef08c300bc9a06be9f20e4b5035045
MalachiteMiller <malachitehmiller@gmail.com>
2023-05-18 20:27:45 -0700

    Add documentation

--- convert_and_upload.py
+++ convert_and_upload.py
@@ -9,9 +9,23 @@ import pandas as pd
 from xlsx2csv import convert_recursive
 
 
-def convert_and_upload(bq_dest, local_source, c_storage_source):
-    bq_load_options = "--autodetect --allow_quoted_newlines=true --schema_update_option=ALLOW_FIELD_ADDITION " \
-                      "--schema_update_option=ALLOW_FIELD_RELAXATION --source_format=CSV"
+def convert_and_upload(bq_dest: str, local_source: str | None, c_storage_source: str | None) -> None:
+    """Converts all xlsx files in a local or gcloud directory to csv and uploads them to a BigQuery table
+
+    Args:
+        bq_dest: The BigQuery table to upload to
+        local_source: The local directory to search for xlsx files
+        c_storage_source: The Cloud Storage directory to search for xlsx files
+
+    Returns:
+        None
+
+    Raises:
+        OSError: Raised when no xlsx files are found in the source directory
+        RuntimeError: Raised when no source is specified
+    """
+    bq_load_options = ("--autodetect --allow_quoted_newlines=true --schema_update_option=ALLOW_FIELD_ADDITION "
+                       "--schema_update_option=ALLOW_FIELD_RELAXATION --source_format=CSV")
     pattern = r"(\d{4})(\d{2})(\d{2})"
 
     output_dir = find_temp_dir()
@@ -24,12 +38,13 @@ def convert_and_upload(bq_dest, local_source, c_storage_source):
                 ending = '/*.xlsx'
             c_storage_source = c_storage_source + ending
         os.system(f"gcloud storage cp {c_storage_source} {input_source}")
-        if len(os.listdir(input_source)) == 0:
-            raise OSError(f"No xlsx files found at {c_storage_source}")
     elif local_source:
         input_source = local_source
     else:
         raise RuntimeError("No source specified")
+
+    if len(os.listdir(input_source)) == 0:
+        raise OSError(f"No xlsx files found at {c_storage_source}")
 
     convert_recursive(input_source, 1, output_dir, {"escape_strings": True})
 
@@ -46,6 +61,9 @@ def convert_and_upload(bq_dest, local_source, c_storage_source):
             df.columns = [re.sub(r'[?\n\r]', '', col) for col in df.columns]
             df['date_exported'] = date
             df.to_csv(file.path, index=False)
+
+            name_no_spaces = file.path.replace(' ', '_')
+            os.rename(file.path, name_no_spaces)
 
     os.system("gcloud storage rm gs://skynamo_history/temp/**")
 
@@ -86,8 +104,8 @@ if __name__ == '__main__':
     parser = ArgumentParser(description='Upload a directory of xlsx files to a BigQuery table')
     parser.add_argument('-t', '--table', help='The output dataset and table name: "dataset.table"')
     parser.add_argument('-l', '--local-source', dest='local_source', default=None,
-                        help='The local directory source: /path/to/directory')
+                        help='Optional: The local directory source: /path/to/directory')
     parser.add_argument('-c', '--cloud-source', dest='cloud_source', default=None,
-                        help='The cloud storage source: gs://bucket/path')
+                        help='Optional: The cloud storage source: gs://bucket/path')
     args = parser.parse_args()
     convert_and_upload(args.table, args.local_source, args.cloud_source)
