bed9ef1812f426be5cf6d696af0459175eee2c0a
Dilshod Temirkhodjaev <tdilshod@gmail.com>
2024-07-29 18:27:36 +0300

    0.8.3

--- xlsx2csv.py
+++ xlsx2csv.py
@@ -24,7 +24,7 @@
 
 __author__ = "Dilshod Temirkhodjaev <tdilshod@gmail.com>"
 __license__ = "MIT"
-__version__ = "0.8.2"
+__version__ = "0.8.3"
 
 import csv, datetime, zipfile, sys, os, re, signal, io
 import xml.parsers.expat
