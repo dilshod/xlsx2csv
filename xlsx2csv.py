f451e6eaba3bdc3cb9329b068875c35d88de87af
Steven Grimm <steven@terraformation.com>
2025-12-09 17:11:14 -0800

    Support percentage rendering
    Format strings for whole and two-decimal-place percentages
    were included in the list of valid format strings, but they
    resolved to a `percentage` format type which didn't have any
    rendering logic.
    
    Fixes #81

--- xlsx2csv.py
+++ xlsx2csv.py
@@ -965,6 +965,16 @@ class Sheet:
                         else:
                             # unsupported float formatting
                             self.data = ("%f" % data).rstrip('0').rstrip('.')
+                    elif format_type == 'percentage':
+                        # Always round .5 up, not to nearest even as round() does.
+                        with localcontext() as ctx:
+                            ctx.rounding = ROUND_HALF_UP
+                            if format_str == "0.00%":
+                                quant = "1.00"
+                            else:
+                                quant = "1"
+                            data = (Decimal(self.data) * 100).quantize(Decimal(quant))
+                            self.data = str(data) + "%"
 
                 except (ValueError, OverflowError):  # this catch must be removed, it's hiding potential problems
                     if self.options['ignore_invalid_char_data']:
